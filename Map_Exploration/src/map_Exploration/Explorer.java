0e8514fe202cbc155460e57209fde58be53dca01
José Vieira <evenilink.code@gmail.com>
2017-10-29 23:12:58 +0000

    First basic explorer.

--- Map_Exploration/src/map_Exploration/Explorer.java
+++ Map_Exploration/src/map_Exploration/Explorer.java
@@ -1,16 +1,65 @@
 package map_Exploration;
 
+import com.sun.media.jai.util.Service;
+
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 import sajas.core.Agent;
+import sajas.core.behaviours.SimpleBehaviour;
+import sajas.domain.DFService;
 
 public class Explorer extends Agent {
 
+	private boolean foundExit = false;
+	
 	@Override
 	public void setup() {
+		DFAgentDescription dfAgentDescription = new DFAgentDescription();
+		dfAgentDescription.setName(getAID());
 		
+		ServiceDescription serviceDescription = new ServiceDescription();
+		serviceDescription.setName(getName());
+		serviceDescription.setType("Explorer");
+		
+		dfAgentDescription.addServices(serviceDescription);
+		
+		try {
+			DFService.register(this, dfAgentDescription);
+		} catch(FIPAException e) {
+			e.printStackTrace();
+		}
+		
+		PledgeBehaviour behaviour = new PledgeBehaviour(this);
+		addBehaviour(behaviour);
 	}
 	
 	@Override
 	public void takeDown() {
+		try {
+			DFService.deregister(this);
+		} catch(FIPAException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	class PledgeBehaviour extends SimpleBehaviour {
+
+		public PledgeBehaviour(Agent a) {
+			super(a);
+		}
+		
+		@Override
+		public void action() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public boolean done() {
+			// TODO Auto-generated method stub
+			return false;
+		}
 		
 	}
 }
