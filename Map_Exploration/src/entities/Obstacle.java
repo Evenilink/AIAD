e5f27f583ed8f3b0c2476e20b94a7caab50c1ed9
Fr0sk <the.fr0sk@gmail.com>
2017-12-04 18:58:28 +0000

    Added number of agents needed to destroy an obstacle

--- Map_Exploration/src/entities/Obstacle.java
+++ Map_Exploration/src/entities/Obstacle.java
@@ -2,13 +2,25 @@ package entities;
 
 public class Obstacle extends Entity {
 	private final boolean seeThrough;
+	private final int neededAgentsForRemoving;
 
-	public Obstacle(int posX, int posY, boolean seeThrough) {
+	public Obstacle(int posX, int posY) {
 		super(posX, posY, 3);
+		this.seeThrough = false;
+		this.neededAgentsForRemoving = 1;
+	}
+
+	public Obstacle(int posX, int posY, int entityCode, boolean seeThrough, int neededAgentsForRemoving) {
+		super(posX, posY, entityCode);
 		this.seeThrough = seeThrough;
+		this.neededAgentsForRemoving = neededAgentsForRemoving;
 	}
 
 	public boolean isSeeThrough() {
 		return seeThrough;
+	}
+
+	public int getNeededAgentsForRemoving() {
+		return neededAgentsForRemoving;
 	}
 }
