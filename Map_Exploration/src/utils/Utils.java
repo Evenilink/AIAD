f515d22856647d8970219982fdd7126912f0246d
Fr0sk <the.fr0sk@gmail.com>
2017-12-04 17:20:51 +0000

    Added Javadoc to the Utils class

--- Map_Exploration/src/utils/Utils.java
+++ Map_Exploration/src/utils/Utils.java
@@ -3,9 +3,8 @@ package utils;
 import repast.simphony.space.grid.GridPoint;
 
 public class Utils {
-	
 	public static final double sqrt2 = Math.sqrt(2);
-	
+
 	public enum MessageType {
 		MATRIX,
 		HELP,
@@ -22,7 +21,13 @@ public class Utils {
 		EXIT,
 		OBSTACLE
 	}
-	
+
+	/**
+	 * Calculates the distance between two 2D points
+	 * @param source The first point
+	 * @param target The second point
+	 * @return float Returns the distance between the two points
+	 */
 	public static float getDistance(Coordinates source, Coordinates target) {
 		int distX = Math.abs(target.getX() - source.getX());
 		int distY = Math.abs(target.getY() - source.getY());
@@ -31,7 +36,12 @@ public class Utils {
 			return (float)sqrt2 * distY + 1 * (distX - distY);
 		return (float)sqrt2 + 1 * (distY - distX);
 	}
-	
+
+	/**
+	 * Gets the highest value of an array
+	 * @param values The int array to search
+	 * @return int Returns the highest calue
+	 */
 	public static int findMax(int[] values) {
 		int max = Integer.MIN_VALUE;
 		
@@ -45,13 +55,20 @@ public class Utils {
 	
 	/**
 	 * Calculates the matrix coordinate from a world point.
-	 * @param point
-	 * @return
+	 * @param point The point to convert from
+	 * @param gridSizeY The height dimension of the grid
+	 * @return Coordinates Returns the matrix coordinates
 	 */
 	public static Coordinates matrixFromWorldPoint(GridPoint point, int gridSizeY) {
 		return new Coordinates(point.getX(), gridSizeY - 1 - point.getY());
 	}
-	
+
+	/**
+	 * Calculates the matrix coordinate from a world point.
+	 * @param coordinates The coordinates to convert from
+	 * @param gridSizeY The height dimension of the grid
+	 * @return Coordinates returns the matrix coordinates
+	 */
 	public static Coordinates worldPointFromMatrix(Coordinates coordinates, int gridSizeY) {
 		return new Coordinates(coordinates.getX(), gridSizeY - 1 - coordinates.getY());
 	}
