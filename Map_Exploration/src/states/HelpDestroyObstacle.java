01c459c56919d5dff2ddda68c3d7314f20a0721d
Huguinho <huguinho27@users.noreply.github.com>
2017-12-07 10:12:03 +0000

    Need help on destroying obtacle

--- Map_Exploration/src/states/HelpDestroyObstacle.java
+++ Map_Exploration/src/states/HelpDestroyObstacle.java
@@ -1,21 +1,30 @@
 package states;
 
+import java.util.List;
 import behaviours.Exploration;
+import communication.*;
+import repast.simphony.query.space.grid.GridCell;
+import sajas.core.AID;
+import utils.Utils;
 
 public class HelpDestroyObstacle implements IAgentState
 {
+	private Exploration behaviour;
+	private int numberAgentsNeeded;
 
 	@Override
 	public void enter(Exploration behaviour)
 	{
-		System.out.println("Hello From HelpDestroyObstacle");
+		//Get minimum number of agents to destroy obstacle
+		//Check if around me there's that number of agents, that can help
+		//if not, go to a random position on the map and recruit
+		//Come back, destroy, and search the obstacle
 	}
 
 	@Override
 	public void execute()
 	{
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
